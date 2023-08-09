e9ce853f81741d44ad72c88f0f4b233a4496e614
Leszek Mielnikow <63610219+LexiestLeszek@users.noreply.github.com>
2023-08-09 13:44:28 +0200

    added case insensitive matching, font pairs file, css

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Hardcoded font pairing mapping
+# Hardcoded font pairing mapping (lowercase keys for case-insensitive matching)
 font_pairing_mapping = {
-    "Arial": "Helvetica",
-    "Times New Roman": "Georgia",
-    "Courier New": "Monospace",
-    # Add more font pairings here
+    "arial": "times",
+    "twisted": "gogo",
 }
 
 @app.get("/", response_class=HTMLResponse)
@@ -20,8 +17,9 @@ async def read_root():
 
 @app.get("/pairing/")
 async def get_pairing(original_font: str):
-    if original_font in font_pairing_mapping:
-        paired_font = font_pairing_mapping[original_font]
-        return {"original_font": original_font, "paired_font": paired_font}
+    original_font_lower = original_font.lower()  # Convert input to lowercase
+    if original_font_lower in font_pairing_mapping:
+        paired_font = font_pairing_mapping[original_font_lower]
+        return {"original_font": original_font_lower, "paired_font": paired_font}
     else:
         raise HTTPException(status_code=404, detail="Font pairing not found")
